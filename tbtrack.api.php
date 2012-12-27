3272cd248b9041bbab7d16c8a999c1531d033e1e
wangxb07 <wangxb07@gmail.com>
2012-12-28 01:00:35 +0800

    add hook_tbtrack_platform_attach_params, implment tbtrack_load_platform_info() and adjust the relevant code.

--- tbtrack.api.php
+++ tbtrack.api.php
@@ -12,11 +12,14 @@ function hook_tbtrack_platform_info() {
   return array(
     'tmall' => array(
       'title' => 'TMall',
-      'search uri' => array(
-        'host' => '',
-        'path' => '',
+      // if your module will implements ranking for platform
+      'ranking' => array(
+        'search uri' => array(
+          'host' => '',
+          'path' => '',
+        ),
+        'selector callback' => '',
       ),
-      'selector callback' => '',
     ),
     'taobao' => array(
       'title' => 'Taobao',
@@ -25,6 +28,27 @@ function hook_tbtrack_platform_info() {
         'path' => '',
       ),
       'selector callback' => '',
+    ),
+  );
+}
+
+/**
+ * Registers your module as an list of platform attach params
+ */
+function hook_tbtrack_platform_attach_params() {
+  return array(
+    'ranking' => array(
+      'search host' => array(
+        '#required' => TRUE,
+        '#default_value' => 'http://taobao.com',
+      ),
+      'search path' => array(
+        '#default_value' => '/search',
+        '#required' => TRUE,
+      ),
+      'selector callback' => array(
+        '#required' => TRUE,
+      ),
     ),
   );
 }
